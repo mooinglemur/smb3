1b2e2fe1d912e99d76b746c819db2719b7db6831
mooinglemur <mooinglemur@users.noreply.github.com>
2025-01-13 23:20:22 -0600

    convert prg013 to ca65

---
+++ asm/levels/Under.asm
@@ -0,0 +1,64 @@
+W502L:		.include "Under/5-2.asm"		; 5-2 (above ground)
+W802L:		.include "Under/8-2.asm"		; 8-2
+W105L:		.include "Under/1-5.asm"		; 1-5
+W308L:		.include "Under/3-8.asm"		; 3-8
+W8HBA:		.include "Under/W8HBA.asm"		; World 8 Hammer Bro battle (I think!)
+PJALL:		.include "Under/PipeJcAL.asm"	; Pipe Junction A (Emerge from left side)
+PJARL:		.include "Under/PipeJcAR.asm"	; Pipe Junction A (Emerge from right side)
+PJBLL:		.include "Under/PipeJcBL.asm"	; Pipe Junction B (Emerge from left side)
+PJBRL:		.include "Under/PipeJcBR.asm"	; Pipe Junction B (Emerge from right side)
+PJCLL:		.include "Under/PipeJcCL.asm"	; Pipe Junction C (Emerge from left side)
+PJCRL:		.include "Under/PipeJcCR.asm"	; Pipe Junction C (Emerge from right side)
+PJDLL:		.include "Under/PipeJcDL.asm"	; Pipe Junction D (Emerge from left side)
+PJDRL:		.include "Under/PipeJcDR.asm"	; Pipe Junction D (Emerge from right side)
+PJELL:		.include "Under/PipeJcEL.asm"	; Pipe Junction E (Emerge from left side)
+PJERL:		.include "Under/PipeJcER.asm"	; Pipe Junction E (Emerge from right side)
+PJFLL:		.include "Under/PipeJcFL.asm"	; Pipe Junction F (Emerge from left side)
+PJFRL:		.include "Under/PipeJcFR.asm"	; Pipe Junction F (Emerge from right side)
+W40F_BonusL:	.include "Under/4-FBonus.asm"	; World 4 fortress bonus area
+BigQBlock1L:	.include "Under/BigQ1.asm"		; World 1's Big [?] block area (empty)
+BigQBlock2L:	.include "Under/BigQ2.asm"		; World 2's Big [?] block area
+BigQBlock3L:	.include "Under/BigQ3.asm"		; World 3's Big [?] block area
+BigQBlock4L:	.include "Under/BigQ4.asm"		; World 4's Big [?] block area
+BigQBlock5L:	.include "Under/BigQ5.asm"		; World 5's Big [?] block area
+BigQBlock6L:	.include "Under/BigQ6.asm"		; World 6's Big [?] block area
+BigQBlock7L:	.include "Under/BigQ7.asm"		; World 7's Big [?] block area
+BigQBlock8L:	.include "Under/BigQ8.asm"		; World 8's Big [?] block area
+W304L:		.include "Under/3-4.asm"		; 3-4
+W608L:		.include "Under/6-8.asm"		; 6-8
+W8HBB:		.include "Under/W8HBB.asm"		; World 8 Hammer Bro battle (I think!), with power up
+EmptyUnder01L:	.include "Under/Empty01.asm"		; Empty/unused
+EmptyUnder02L:	.include "Under/Empty02.asm"		; Empty/unused
+EmptyUnder03L:	.include "Under/Empty03.asm"		; Empty/unused
+EmptyUnder04L:	.include "Under/Empty04.asm"		; Empty/unused
+EmptyUnder05L:	.include "Under/Empty05.asm"		; Empty/unused
+EmptyUnder06L:	.include "Under/Empty06.asm"		; Empty/unused
+EmptyUnder07L:	.include "Under/Empty07.asm"		; Empty/unused
+EmptyUnder08L:	.include "Under/Empty08.asm"		; Empty/unused
+EmptyUnder09L:	.include "Under/Empty09.asm"		; Empty/unused
+EmptyUnder10L:	.include "Under/Empty10.asm"		; Empty/unused
+EmptyUnder11L:	.include "Under/Empty11.asm"		; Empty/unused
+EmptyUnder12L:	.include "Under/Empty12.asm"		; Empty/unused
+EmptyUnder13L:	.include "Under/Empty13.asm"		; Empty/unused
+EmptyUnder14L:	.include "Under/Empty14.asm"		; Empty/unused
+EmptyUnder15L:	.include "Under/Empty15.asm"		; Empty/unused
+EmptyUnder16L:	.include "Under/Empty16.asm"		; Empty/unused
+EmptyUnder17L:	.include "Under/Empty17.asm"		; Empty/unused
+EmptyUnder18L:	.include "Under/Empty18.asm"		; Empty/unused
+EmptyUnder19L:	.include "Under/Empty19.asm"		; Empty/unused
+EmptyUnder20L:	.include "Under/Empty20.asm"		; Empty/unused
+EmptyUnder21L:	.include "Under/Empty21.asm"		; Empty/unused
+EmptyUnder22L:	.include "Under/Empty22.asm"		; Empty/unused
+EmptyUnder23L:	.include "Under/Empty23.asm"		; Empty/unused
+EmptyUnder24L:	.include "Under/Empty24.asm"		; Empty/unused
+EmptyUnder25L:	.include "Under/Empty25.asm"		; Empty/unused
+EmptyUnder26L:	.include "Under/Empty26.asm"		; Empty/unused
+EmptyUnder27L:	.include "Under/Empty27.asm"		; Empty/unused
+EmptyUnder28L:	.include "Under/Empty28.asm"		; Empty/unused
+EmptyUnder29L:	.include "Under/Empty29.asm"		; Empty/unused
+EmptyUnder30L:	.include "Under/Empty30.asm"		; Empty/unused
+EmptyUnder31L:	.include "Under/Empty31.asm"		; Empty/unused
+EmptyUnder32L:	.include "Under/Empty32.asm"		; Empty/unused
+EmptyUnder33L:	.include "Under/Empty33.asm"		; Empty/unused
+EmptyUnder34L:	.include "Under/Empty34.asm"		; Empty/unused
+EmptyUnder35L:	.include "Under/Empty35.asm"		; Empty/unused
